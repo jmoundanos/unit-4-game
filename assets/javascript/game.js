bc19b652140a438d298c9cadeceb7017da39787d
Jennifer Moundanos <jmoundanos@gmail.com>
2020-01-03 14:49:27 -0600

    finished game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,78 @@
 
+$(document).ready(function(){
 
 var wins = 0;
 var losses = 0;
 var currentTotal = 0;
-
-
+var redRandom = 0;
+var blueRandom = 0;
+var yellowRandom = 0;
+var greenRandom = 0;
 
 window.onload = function(){
     var randomNumber = Math.floor(Math.random() * 120) + 19;
     $("#randomNumber").text(randomNumber);
-};
-    var redRandom = Math.floor(Math.random() * 12) + 1;
-    console.log(redRandom);
-    var blueRandom = Math.floor(Math.random() * 12) + 1;
-    console.log(blueRandom);
-    var yellowRandom = Math.floor(Math.random() * 12) + 1;
-    console.log(yellowRandom);
-    var greenRandom = Math.floor(Math.random() * 12) + 1;
-    console.log(greenRandom);
-
-    function playGame(currentTotal){
-        var num1 = $("#randomNumber").text();
-        var num2 = currentTotal;
-        if(num1 < num2){
-            losses++;
-            $("#losses").text("Losses: " + losses);
-            alert("You lose!");
-            reset();
-        }else if(num1 === num2){
-            wins++;
-            $("#wins").tex1t("Wins: " + wins);
-            alert("You win")
-            
-            reset();
     
-        }
-    }
+};
+function setCrystals(){
+    redRandom = Math.floor(Math.random() * 12) + 1;
+    console.log(redRandom);
+    blueRandom = Math.floor(Math.random() * 12) + 1;
+    console.log(blueRandom);
+    yellowRandom = Math.floor(Math.random() * 12) + 1;
+    console.log(yellowRandom);
+    greenRandom = Math.floor(Math.random() * 12) + 1;
+    console.log(greenRandom);
+}
 
-    $("#redCrystal").on("click", function(){
+setCrystals();
+
+function playGame(currentTotal){
+    var num1 = $("#randomNumber").text();
+    var num2 = currentTotal;
+    if(parseInt(num1) < parseInt(num2)){
+        $("#currentTotal").text(currentTotal);
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        $("#loser").html("<p>You lose!</p>");
+        reset();
+    }
+    if(parseInt(num1) === parseInt(num2)){
+        $("#currentTotal").text(currentTotal);
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        $("#winner").html("<p>You Win!</p>");
+        reset();
+    }
+}
+function reset(){
+    currentTotal = 0;
+    $("#currentTotal").text(currentTotal);
+    var randomNumber = Math.floor(Math.random() * 120) + 19;
+    $("#randomNumber").text(randomNumber);
+    $("#loser").html("<p></p>");
+    $("#winner").html("<p></p>");
+    setCrystals();
+}
+$("#redCrystal").on("click", function(){
         currentTotal = currentTotal + redRandom;
         $("#currentTotal").text(currentTotal);
         playGame(currentTotal);
     })
-    $("#blueCrystal").on("click", function(){
+$("#blueCrystal").on("click", function(){
         currentTotal = currentTotal + blueRandom;
         $("#currentTotal").text(currentTotal);
         playGame(currentTotal);
     })
-    $("#yellowCrystal").on("click", function(){
+$("#yellowCrystal").on("click", function(){
         currentTotal = currentTotal + yellowRandom;
         $("#currentTotal").text(currentTotal);
         playGame(currentTotal);
     })
-    $("#greenCrystal").on("click", function(){
+$("#greenCrystal").on("click", function(){
         currentTotal = currentTotal + greenRandom;
         $("#currentTotal").text(currentTotal);
         playGame(currentTotal);
     })
-    function reset(){
-        currentTotal = 0;
-        var randomNumber = Math.floor(Math.random() * 120) + 19;
-        $("#randomNumber").text(randomNumber);
-    }
-    
-    
-
+});
  
